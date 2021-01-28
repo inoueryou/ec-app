2d1e9ce7f13993a66ed33001b8e85cb7ea8874f6
inouekatou <ino0607soccer@gmail.com>
2021-01-28 22:51:02 +0900

    create signin and signup

--- src/reducks/users/operations.js
+++ src/reducks/users/operations.js
@@ -4,27 +4,35 @@ import {auth, db, firebaseTimestamp} from '../../firebase/index'
 
 
 
-export const signIn = () => {
-  return async (dispatch, getState) => {
-    const state = getState()
-    const isSignedIn = state.users.isSignedIn
+export const signIn = (email, password) => {
+  return async (dispatch) => {
 
-    if (!isSignedIn) {
-      const url = 'https://api.github.com/users/inoueryou'
-
-      const response = await fetch(url)
-       .then(res => res.json())
-       .catch(() => null)
-
-       const username = response.login
-
-       dispatch(signInAction({
-        isSignedIn: true,
-        uid: '0001',
-        username: username
-       }))
-       dispatch(push('/'))
+    if (email === '' || password === '') {
+      alert('必須項目が未入力です')
+      return false
     }
+
+    return auth.signInWithEmailAndPassword (email, password)
+      .then(result => {
+        const user = result.user
+
+        if (user) {
+          const uid = user.uid
+
+          db.collection('users').doc(uid).get()
+          .then((snapshot) => {
+            const data = snapshot.data()
+
+            dispatch(signInAction({
+              isSignedIn: true,
+              role: data.role,
+              uid: uid,
+              username: data.username
+            }))
+            dispatch(push('/'))
+          })
+        }
+      })
   }
 }
 
